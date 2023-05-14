ae4235bc4b3dafe80294d45549f278fbde97567d
carlius404 <siniscalchi404@gmail.com>
2023-05-14 08:06:20 +0200

    get transfer values from hash

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 app=Flask(__name__)
 
 @app.route("/api/analyze/<wallet>")
 def wallet(wallet):
-    return "PROVOLA"+wallet
+
+    return 
 
 if __name__=="__main__":
     app.run(debug=True)
