e5a1911589b6a62ce5121f3fcfcda595e0a3da8e
carlius404 <siniscalchi404@gmail.com>
2023-05-14 12:01:35 +0200

    get transfer value from a transfer value

--- backend/getData.py
+++ backend/getData.py
@@ -4,11 +4,22 @@ import requests
 import json
 from web3 import Web3
 
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 load_dotenv()
 scanKey = os.getenv('ETHERSCAN_KEY')
 plorerKey = os.getenv('ETHPLORER_KEY')
+
 def getTxs(address):
     url=f'https://api.etherscan.io/api?module=account&action=txlist&address={address}&startblock=0&endblock=99999999&sort=asc&apiKey={scanKey}'
     res=requests.get(url)
@@ -27,17 +38,29 @@ def getValues(txHash):
     url=f'https://api.ethplorer.io/getTxInfo/{txHash}?apiKey={plorerKey}'
     res=requests.get(url)
     res=json.loads(res.text)
-    print(res)
+    fromAddress=res['from']
+    
+    if 'operations' not in res:
+        return
     operations=res['operations']
-
+    
     for operation in operations:
-        print(operation)
+        if operation['type']=='transfer':
+            color='\u001b[0m'
+            
+            if fromAddress==operation['from']:
+                color=colors.FAIL
+            if fromAddress==operation['to']:
+                color=colors.GREEN
+            print(color+operation['value']+" "+operation['tokenInfo']['symbol'])
 
 def getTrades(address):
     txs=getTxs(address)
     for tx in txs:
-        logs=getValues(tx)
+        print(colors.BLUE+tx['hash'])
+        logs=getValues(tx['hash'])
+        print()
 
     
 #0xF9a5844AC0b81Bc45f448F864D484E0EF5927D5f
-getValues("0xc1005cbf96fc4415f216eadf4551cb3552abbfa732400d001dfa0b7056c1b7d7")
+getTrades("0xF9a5844AC0b81Bc45f448F864D484E0EF5927D5f")
