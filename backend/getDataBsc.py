f0b42338c87b0fb16f1ea659d1fd57af3b76d3d3
carlius404 <siniscalchi404@gmail.com>
2023-05-17 21:56:45 +0200

    succesfully getting price pf a token

--- backend/getDataBsc.py
+++ backend/getDataBsc.py
@@ -6,8 +6,8 @@ from web3 import Web3
 
 load_dotenv()
 scanKey = os.getenv('BSCSCAN_KEY')
-
-
+web3 = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
+stableCoins=["0x55d398326f99059ff775485246999027b3197955","0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c","0xe9e7cea3dedca5984780bafc599bd69add087d56"]
 def getTxs(address):
     url=f'https://api.bscscan.com/api?module=account&action=txlist&address={address}&startblock=0&endblock=99999999&sort=asc&apiKey={scanKey}'
     res=requests.get(url)
@@ -33,7 +33,57 @@ def getAbi(address):
     url=f'https://api.bscscan.com/api?module=contract&action=getsourcecode&address={address}&apikey={scanKey}'
     res=requests.get(url)
     res=json.loads(res.text)
+    res=res['result']
     if len(res)>1:
         print(f"WARNING: more than one contract was returned from {address} ({len(res)})")
-    res=res['result']
-    return res
+    return res[0]['ABI']
+
+def getPrice(address,txHash):
+    logs=web3.eth.get_transaction_receipt(txHash).logs
+    abi=getAbi(address)
+    contract=web3.eth.contract(address, abi=abi)
+
+    #recognize which one of the two tokens is the stable coin, 
+    #so we can determine the price of the token compared to the stable coin
+
+    token0=contract.functions.token0().call()
+    token1=contract.functions.token1().call()
+    
+    if token0.lower() in stableCoins:
+        stable=0
+        token=1
+    if token1.lower() in stableCoins:
+        stable=1
+        token=0
+    
+    #create a dict that maps all the events signature to the name of the event
+
+    hexToName={}
+    for abi in contract.abi:
+        if abi["type"] == "event":
+            name = abi["name"]
+            inputs = [param["type"] for param in abi["inputs"]]
+            inputs = ",".join(inputs)
+            
+            signatureText=f"{name}({inputs})"
+            signatureHex=web3.toHex(web3.keccak(text=signatureText))
+            hexToName[signatureHex[:10]]=name
+    buyPrices=[]
+    sellPrices=[]
+    for log in logs:
+        if log['address'].lower()==address.lower():
+            topic0=log['topics'][0].hex()
+            topic0=topic0[:10]
+            if topic0=="0xd78ad95f": #if the topic0 has the signature of the swap event
+                    decodedLogs=contract.events[hexToName[topic0]]().processLog(log)
+                    stableIn=decodedLogs['args'][f'amount{stable}In']
+                    stableOut=decodedLogs['args'][f'amount{stable}Out']
+                    tokenIn=decodedLogs['args'][f'amount{token}In']
+                    tokenOut=decodedLogs['args'][f'amount{token}Out']
+                    if stableIn!=0:
+                        buyPrices.append(stableIn/tokenOut)
+                    else:
+                        sellPrices.append(stableOut/tokenIn)
+    return buyPrices,sellPrices
+
+                    
--- backend/tokenHistory.py
+++ backend/tokenHistory.py
@@ -1,17 +1,24 @@
 from getDataBsc import *
+from web3 import Web3
+import matplotlib.pyplot as plt
 
 def swapValues(logs):
     for log in logs:
         for k in log:
             print(1)   
 def getPrices(pairAddress):
-    abi=getAbi(pairAddress)
-    print(abi[0])
-    print(abi[1])
-    print(abi[2])
     txs=getTokenTxs(pairAddress)
+    buyPrices=[]
+    sellPrices=[]
+    doneTxs=[]
     for tx in txs:
-        logs=getLogs(tx['hash'])
-        
-
-getPrices("0xa70fC90D49e326dabF4eF63C1a77de063C5ab4d0")
+        if tx['hash'] not in doneTxs:
+            doneTxs.append(tx['hash'])
+            print(len(buyPrices))
+            buy,sell=getPrice(pairAddress,tx['hash'])
+            buyPrices+=buy
+            sellPrices+=sell
+            if len(buyPrices)>100:
+                plt.plot(buyPrices)
+                plt.show()
+getPrices("0xb7365f296b71094B57662bd357a2cd29282Bb133")
